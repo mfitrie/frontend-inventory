c0aeb99a3a0dcfd8a099f5a62965a9b7fbbcc703
Fitrie <mfitrie78@gmail.com>
2024-03-14 22:43:09 +0800

    fix: add serverurl live backend url to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    env: {
+        serverUrl: "https://backend-inventory-delta.vercel.app",
+    }
 };
 
 export default nextConfig;
