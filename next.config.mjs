76ee533a8fe262b0283cb298cac43399e8713265
Fitrie <mfitrie78@gmail.com>
2024-03-15 13:22:41 +0800

    feat: add prod backend link

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        serverUrl: "https://backend-inventory-git-release-mfitrie78-gmailcom.vercel.app",
+        // serverUrl: "https://backend-inventory-git-release-mfitrie78-gmailcom.vercel.app",
+        // serverUrl: "http://localhost:4000",
+        serverUrl: "https://backend-inventory-delta.vercel.app",
     }
 };
 
